0d8565e9ff6b59cc1202f06a1d18e15ac9bb9cc8
niva0001 <nivashreemasala@gmail.com>
2026-07-14 20:14:42 +0530

    Add sabudana vada recipe

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -1,6 +1,7 @@
 import kheer from "../assets/recipes/sabudana-kheer.jpg";
 import khichdi from "../assets/recipes/sabudana-khichdi.jpg";
 import jeeraRice from "../assets/recipes/jeera-rice.jpg";
+import vada from "../assets/recipes/sabudana-vada.jpg";
 
 const recipes = [
   {
@@ -10,19 +11,29 @@ const recipes = [
     description:
       "A creamy dessert prepared with Nivashree Premium Sabudana, milk, dry fruits and cardamom.",
   },
+
   {
     id: 2,
     name: "Sabudana Khichdi",
     image: khichdi,
     description:
-      "A light, delicious and healthy fasting recipe made with premium sabudana.",
+      "A light, delicious and healthy fasting recipe made with premium sabudana, peanuts and spices.",
   },
+
   {
     id: 3,
     name: "Jeera Rice",
     image: jeeraRice,
     description:
-      "Fragrant basmati rice cooked with aromatic Nivashree Premium Jeera.",
+      "Fragrant basmati rice cooked with aromatic Nivashree Premium Jeera for a delicious meal.",
+  },
+
+  {
+    id: 4,
+    name: "Sabudana Vada",
+    image: vada,
+    description:
+      "Crispy and golden vrat-special Sabudana Vada made with Nivashree Premium Sabudana, peanuts and spices.",
   },
 ];
 
