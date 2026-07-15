8fcd02daac325ebc60c5164a679a9fef031f6a60
niva0001 <nivashreemasala@gmail.com>
2026-07-15 09:59:55 +0530

    Add Open Graph and Twitter SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,19 +1,40 @@
 import { Helmet } from "react-helmet-async";
 
-function SEO({ title, description }) {
+function SEO({
+  title,
+  description,
+  image = "https://nivashreemasala.com/logo.png",
+  url = "https://nivashreemasala.com",
+}) {
   return (
     <Helmet>
+      {/* Basic SEO */}
       <title>{title}</title>
 
-      <meta
-        name="description"
-        content={description}
-      />
+      <meta name="description" content={description} />
 
       <meta
         name="keywords"
-        content="Nivashree Masala, Premium Sabudana, Sabudana, Jeera, Indian Food Products"
+        content="Nivashree Masala, Premium Sabudana, Sabudana, Jeera, Tej Patta, Nakul Dana, Cutting Mishri, Indian Food Products"
       />
+
+      <meta name="author" content="Nivashree Masala" />
+
+      <link rel="canonical" href={url} />
+
+      {/* Open Graph */}
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Nivashree Masala" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
     </Helmet>
   );
 }
